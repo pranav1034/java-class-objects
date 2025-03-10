0bf1d0474f7fdcf9b6ec0d371ffb82d735eff281
pranav1034 <pranavaggarwal04@gmail.com>
2025-03-10 13:44:40 +0530

    [Pranav] ADD display-details-of-circle

---
+++ Circle.java
@@ -0,0 +1,29 @@
+public class Circle {
+
+    final private double radius;
+
+    public Circle(double radius){
+        this.radius=radius;
+    }
+    public double calculateArea(){
+        return Math.PI*radius*radius;
+    }
+    public double calculateCircumference(){
+        return 2*Math.PI*radius;
+    }
+
+    public void displayDetails(){
+        System.out.println("Radius: "+radius);
+        System.out.println("Area: "+calculateArea());
+        System.out.println("Circumference: "+calculateCircumference());
+    }
+
+    public static void main(String[] args) {
+        Circle circle1=new Circle(14);
+        Circle circle2=new Circle(11);
+        System.out.println("The details of circle1 is: ");
+        circle1.displayDetails();
+        System.out.println("The details of circle2 is: ");
+        circle2.displayDetails();
+    }
+}
